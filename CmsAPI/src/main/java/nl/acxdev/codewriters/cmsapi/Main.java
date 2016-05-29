6a016e9830d3c36f6abd09ac9fc3d9f9c5877b30
Absentium <j.kraaijkamp@acxdev.nl>
2016-05-29 16:01:04 +0200

    Placed correct code to git

---
+++ CmsAPI/src/main/java/nl/acxdev/codewriters/cmsapi/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.acxdev.codewriters.cmsapi;
+
+import static spark.Spark.*;
+
+/**
+ *
+ * @author absentium
+ */
+public class Main {
+    public static void main(String[] args){
+        // Get the servers with the containers in the servers
+        get("/serverList", (req,res) -> "Get the server list");
+        
+        // Start or stop a specific container
+        get("/start/:cId/:cType", (req, res) -> "Start container with id " + req.params("cId") + " of type " + req.params("cType"));
+        get("/stop/:cId/:cType", (req, res) -> "Stop container with id " + req.params("cId") + " of type " + req.params("cType"));
+        
+        // Update a specific container
+        get("/rename/:cId/:cType/:cNewName", (req, res) -> "Rename container with id " + req.params("cId") + " of type " + req.params("cType") + " to " + req.params("cNewName"));
+        get("/move/:cId/:cType/:cDestination", (req, res) -> "Move container with id " + req.params("cId") + " of type " + req.params("cType") + " to " + req.params("cDestination"));
+        
+        // Remove a specific container
+        get("/remove/:cId/:cType", (req, res) -> "Remove container with id " + req.params("cId") + " of type" + req.params("cType"));
+        
+        // Create a new container on a specific server
+        get("/create/:cDestination/:image/:cName/:port", (req, res) -> "Create a new container on " + req.params("cDestination") + " with the " + req.params("image") + " image, name it " + req.params("cName") + " and use these " + req.params("port") + " port");
+    }
+}
